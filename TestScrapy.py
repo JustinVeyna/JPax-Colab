de0da35719cbedfa20943e01e5b1e8f94c083d3e
Justin Veyna <jcveyna@uci.edu>
2017-06-23 13:40:12 -0700

    started test crawler

---
+++ TestScrapy.py
@@ -0,0 +1,19 @@
+'''
+Created on Jun 23, 2017
+
+@author: Shintomo
+'''
+import scrapy
+#scrapy runspider quotes_spider.py -o results.json
+START_URLS = ["https://gelbooru.com/index.php?page=post&s=list&tags=matsushima_michiru"]
+class crawler(scrapy.Spider):
+    def __init__(self):
+        pass
+    def parse(self, response):
+        pass
+            
+
+if __name__ == '__main__':
+    response = scrapy.Selector()
+    for img_obj in respose.css('span.thumb'):
+            yield {'id': img_obj.css('::attr(id)')}
