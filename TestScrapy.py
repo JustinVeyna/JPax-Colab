ae98981be95443f7c37fdd70cbc577fd600e2c76
Justin Veyna <jcveyna@uci.edu>
2017-06-25 16:33:12 -0700

    Can now download images

--- TestScrapy.py
+++ TestScrapy.py
@@ -4,25 +4,38 @@ Created on Jun 23, 2017
 @author: Shintomo
 '''
 import scrapy
-
+from urllib.parse import urljoin
 TEST = False
-
-scrapy.IMAGES_STORE = '/mtsma'
 
 #scrapy runspider quotes_spider.py -o results.json
 START_URLS = ["https://gelbooru.com/index.php?page=post&s=list&tags=matsushima_michiru"]
+
+
+
 class crawler(scrapy.Spider):
-    name = "JPAX crawler"
+    name = "JPAX"
     start_urls = START_URLS
+    custom_settings = {
+        'BOT_NAME': 'JPAX',
+        'IMAGES_STORE':'mtsma',
+        'ITEM_PIPELINES':{'scrapy.pipelines.images.ImagesPipeline': 1},
+        'DOWNLOAD_DELAY':.5
+    }
+    
     def parse(self, response):
         for img_obj in response.css('span.thumb'):
-            d = {'id': img_obj.css('span::attr(id)').extract(), 'tumbnail': img_obj.css('img::attr(src)').extract()}
+            id = img_obj.css('span::attr(id)').extract_first()#image id
+            tmb = img_obj.css('img::attr(src)').extract_first()#thumbnail reletive link
+                        
+            u = urljoin(response.url, tmb) #url of thumbnail
+            i = {'image_urls': [u]}
+            yield i #sends request to save thumbnail
             
-            yield d
-            
+            d = {'id': id, 'tumbnail': tmb}           
+            yield d #sends request to save info
 
 if __name__ == '__main__':
-    if TEST:
+    if TEST: #will test link selection
         response = None
         with open('GelbooruSamplePageMatsushima', 'r') as f:
             response = scrapy.Selector(text = f.read())
