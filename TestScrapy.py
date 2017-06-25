74454832255e85b4a4defe2892812bfe4ed61d06
Justin Veyna <jcveyna@uci.edu>
2017-06-25 10:56:53 -0700

    working crawler
    updated gitignore to ignore json files
    crawler now works for 1 page and no image retrival

--- TestScrapy.py
+++ TestScrapy.py
@@ -4,18 +4,27 @@ Created on Jun 23, 2017
 @author: Shintomo
 '''
 import scrapy
+
+TEST = False
+
+scrapy.IMAGES_STORE = '/mtsma'
+
 #scrapy runspider quotes_spider.py -o results.json
 START_URLS = ["https://gelbooru.com/index.php?page=post&s=list&tags=matsushima_michiru"]
 class crawler(scrapy.Spider):
-    def __init__(self):
-        pass
+    name = "JPAX crawler"
+    start_urls = START_URLS
     def parse(self, response):
-        pass
+        for img_obj in response.css('span.thumb'):
+            d = {'id': img_obj.css('span::attr(id)').extract(), 'tumbnail': img_obj.css('img::attr(src)').extract()}
+            
+            yield d
             
 
 if __name__ == '__main__':
-    response = None
-    with open('GelbooruSamplePageMatsushima', 'r') as f:
-        response = scrapy.Selector(f.read())
-    for img_obj in respose.css('span.thumb'):
-            yield {'id': img_obj.css('::attr(id)')}
+    if TEST:
+        response = None
+        with open('GelbooruSamplePageMatsushima', 'r') as f:
+            response = scrapy.Selector(text = f.read())
+        for img_obj in response.css('span.thumb'):
+                print({'id': img_obj.css('span::attr(id)').extract(), 'tumbnail': img_obj.css('img::attr(src)').extract()})
