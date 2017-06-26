01ced306850e4334ea2bd3b061e2b2a67e827020
PaxtonWong <wongpn@uci.edu>
2017-06-25 23:57:23 -0700

    Modified CrawlerConstants to add PID

--- CrawlerConstants.py
+++ CrawlerConstants.py
@@ -4,6 +4,8 @@ Created on Jun 25, 2017
 @author: Shintomo
 '''
 from urllib.parse import parse_qs
+from collections import defaultdict
+import ProgressLoader as PL
 
 IMAGE_CAP=200
 
@@ -17,6 +19,7 @@ CRAWLER_SETTINGS = {
 START_URLS = []
 START_URLS_OLD = []
 
+PROGRESS_FILE = "SavedProgress.p"
 
 characters = ["matsushima_michiru", "kousaka_kirino"]
 
@@ -30,9 +33,26 @@ host = hosts[1]
 base_url = "https://"+host+"/index.php?page=dapi&s=post&q=index&tags="
 base_url_old = "https://"+host+"/index.php?page=post&s=list&tags="
 
-for character in characters:
-    START_URLS.append(base_url+character)
-    START_URLS_OLD.append(base_url_old+character)
-    
+char_dict = defaultdict(int)
+
+if PL.load_progress_from_file() != None:
+    for character in characters:
+        START_URLS.append(base_url+character+"&pid="+char_dict[character])
+        START_URLS_OLD.append(base_url_old+character+"&pid="+char_dict[character])
+else:
+    for char in characters:
+        START_URLS.append(base_url+char+"&pid=0")
+        START_URLS_OLD.append(base_url_old+char+"&pid=0")
+        
+
 def get_character_from_url(url):
     return parse_qs(url)["tags"][0]
+def get_PID_from_url(url):
+    return parse_qs(url)["pid"][0]
+
+#initialize if not created
+#if initialized, make sure start url includes PID
+#if not exist, initialize each char to 0
+#if file exists, load dictionary informations, append PID from dictionary
+#to the URL
+
